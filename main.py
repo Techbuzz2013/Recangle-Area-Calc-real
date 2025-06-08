e5eef3bec902f41cc84fd94b31413121f42c26cf
Techbuzz2013 <tarunkumarm2013@gmail.com>
2025-06-08 21:14:45 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Taking Input
+
+Length = int(input("Enter The length in cm?: "))
+Width  = int(input("Enter The Width in cm?: "))
+
+# Calculation
+
+Area = (Width * Length)
+
+# Printing the result
+
+print(f"The area of rectangle is {Area}cm²")
